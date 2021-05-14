da2a873ce1a9ad255c1d2f15e79a4507801cd7a4
MasashiAkahori <il_pendolino.masa@docomo.ne.jp>
2021-05-14 15:43:41 +0900

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by 赤堀雅司 on 14/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
